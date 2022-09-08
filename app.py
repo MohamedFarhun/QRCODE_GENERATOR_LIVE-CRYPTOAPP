fe52d0d89dc78b84a6591c28b6d09b25590220db
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-09-09 00:54:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,22 +3,27 @@ from PIL import Image
 import time
 image=("infocryptoapp.png")
 def st_ui():
+    st.balloons()   
     st.title("QR_CODE_GENERATOR_INFOCRYPTO_APP")
     st.caption("Get All The Info Regarding Your Favorite Crypto Currency")
     st.info("Developed by MOHAMED FARHUN M, NANDHAKUMAR S, DHIVAKAR S [Daisi Hackathon]", icon="©")
     st.header("Scan this Generated QRcode below to redirect into our Infocrypto Tracking Project")
-    my_bar = st.progress(0)
-    for percent_complete in range(100):
-        time.sleep(0.01)
-        my_bar.progress(percent_complete + 1)
-    col1, col2, col3 = st.columns([3,6,3])
-    with col1:
-        st.write("")
-    with col2:
-        st.image(image, caption='Live Crypto Tracking App')
-    with col3:
-        st.write("")
-    st.balloons()    
+    
+    if st.button('Generate'):
+        my_bar = st.progress(0)
+        for percent_complete in range(100):
+            time.sleep(0.01)
+            my_bar.progress(percent_complete + 1)
+        col1, col2, col3 = st.columns([3,6,3])
+        with col1:
+            st.write("")
+        with col2:
+            st.image(image, caption='Live Crypto Tracking App')
+        with col3:
+            st.write("")        
+     else:
+     
+ 
    
 if __name__ == "__main__":
     st_ui()
