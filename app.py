1153eeeb14905664ebfb19f05621ff1c08c09d07
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-09-09 00:35:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import streamlit as st
 from PIL import Image
-
+image=("infocryptoapp.png")
 def st_ui():
     st.title("QR_CODE_GENERATOR_INFOCRYPTO_APP")
     st.caption("Get All The Info Regarding Your Favorite Crypto Currency")
     st.info("Developed by MOHAMED FARHUN M, NANDHAKUMAR S, DHIVAKAR S [Daisi Hackathon]")
     st.header("Scan this Generated QRcode below to redirect into our Infocrypto Tracking Project")
-    st.image(image, caption='Live Crypto Tracking App')
-    st.balloons()
     col1, col2, col3 = st.columns([1,6,1])
 
     with col1:
@@ -18,7 +16,11 @@ def st_ui():
 
     with col3:
         st.write("")
+    st.image(image, caption='Live Crypto Tracking App')
+    st.balloons()
+    
 
+   
 if __name__ == "__main__":
     st_ui()
 
