7eda71c4b91c863d76f561d5c9feca59946c9df4
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-09-09 01:00:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,19 +10,16 @@ def st_ui():
     st.header("Scan this Generated QRcode below to redirect into our Infocrypto Tracking Project")
     
     if st.button('Generate'):
-        my_bar = st.progress(0)
-        for percent_complete in range(100):
-            time.sleep(0.01)
-            my_bar.progress(percent_complete + 1)
         col1, col2, col3 = st.columns([3,6,3])
         with col1:
             st.write("")
         with col2:
             st.image(image, caption='Live Crypto Tracking App')
         with col3:
-            st.write("")        
-      else
-          break:
+            st.write("")  
+            
+      else:
+          st.write("Click to generate")
  
    
 if __name__ == "__main__":
