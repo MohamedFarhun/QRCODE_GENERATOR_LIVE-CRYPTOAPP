bdeb2159194519c291a847812c0d0583564c28ab
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-09-09 01:38:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,13 @@ import streamlit as st
 from PIL import Image
 import time
 image=("infocryptoapp.png")
-def st_ui():
-    st.balloons()   
+def st_ui(): 
     st.title("QR_CODE_GENERATOR_INFOCRYPTO_APP")
     st.caption(" Get All The Info Regarding Your Favorite Crypto Currency")
     st.info("Developed by MOHAMED FARHUN M, NANDHAKUMAR S, DHIVAKAR S [Daisi Hackathon]", icon="©")
     st.header("Scan this Generated QRcode below to redirect into our Infocrypto Tracking Project")
     with st.spinner('Generating...'):
         time.sleep(3)
-    st.success('Done!')
     col1, col2, col3 = st.columns([3,6,3])
     with col1:
         st.write("")
@@ -18,7 +16,7 @@ def st_ui():
         st.image(image, caption='Live Crypto Tracking App')
     with col3:
         st.write("")  
-    st.success('QR code generated successfully!', icon="✅")
+    st.success('QR code generated successfully!', *, icon= "✅")
 with st.sidebar:
     st.header("TEKKYZZ")
     st.write("Leader   : MOHAMED FARHUN M")
