77b41ebc8fc548cf355a5b7f4e56a0387aa72301
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-09-09 01:07:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,13 +12,13 @@ def st_ui():
     for percent_complete in range(100):
          time.sleep(0.01)
          my_bar.progress(percent_complete + 1)
-         col1, col2, col3 = st.columns([3,6,3])
-         with col1:
-             st.write("")
-         with col2:
-             st.image(image, caption='Live Crypto Tracking App')
-         with col3:
-             st.write("")  
+    col1, col2, col3 = st.columns([3,6,3])
+    with col1:
+        st.write("")
+    with col2:
+        st.image(image, caption='Live Crypto Tracking App')
+    with col3:
+        st.write("")  
    
 if __name__ == "__main__":
     st_ui()
