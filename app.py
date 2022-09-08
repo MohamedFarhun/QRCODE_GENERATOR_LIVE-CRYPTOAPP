cc17a49fabf744ad4a738d827ba13e2d717eb8ec
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-09-08 21:46:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -125,9 +125,6 @@ def st_ui():
     with st.container():
         st.write("This is inside the container")
 
-    # You can call any Streamlit command, including custom components:
-        st.bar_chart(np.random.randn(50, 3))
-
 st.write("This is outside the container")
 
 
