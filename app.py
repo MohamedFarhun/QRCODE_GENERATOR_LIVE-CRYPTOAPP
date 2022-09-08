205b27a08c1f959a8dfe377f51ff241f3bd39bb8
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-09-08 21:39:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -120,10 +120,10 @@ def st_ui():
     st.image(image, caption='Live Crypto Tracking App')
     st.balloons()
     with st.container():
-    st.write("This is inside the container")
+        st.write("This is inside the container")
 
     # You can call any Streamlit command, including custom components:
-    st.bar_chart(np.random.randn(50, 3))
+        st.bar_chart(np.random.randn(50, 3))
 
 st.write("This is outside the container")
 
