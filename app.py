cae476efa27d5a36390b6750ef8041895b549a50
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-09-09 00:33:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ def st_ui():
     st.caption("Get All The Info Regarding Your Favorite Crypto Currency")
     st.info("Developed by MOHAMED FARHUN M, NANDHAKUMAR S, DHIVAKAR S [Daisi Hackathon]")
     st.header("Scan this Generated QRcode below to redirect into our Infocrypto Tracking Project")
-
+    st.image(image, caption='Live Crypto Tracking App')
+    st.balloons()
     col1, col2, col3 = st.columns([1,6,1])
 
     with col1:
@@ -17,8 +18,7 @@ def st_ui():
 
     with col3:
         st.write("")
-    st.image(image, caption='Live Crypto Tracking App')
-    st.balloons()
+
 if __name__ == "__main__":
     st_ui()
 
