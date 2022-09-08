a5962a8b2d276013da2a2103f924a35abebccc2a
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2022-09-09 02:16:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,17 +24,17 @@ with st.sidebar:
     st.write("Member 1 : NANDHAKUMAR S")
     st.write("Member 2 : DHIVAKAR S")
     st.subheader("**_Do_you_like_our_Project_?**")
-        col1, col2 = st.columns([1,1])
-        with col1:
-            with st.expander("YES"):
-                st.write("""
-                    Thanks for your valuable feedback.
-                """)
-        with col2:
-            with st.expander("NO"):
-                st.write("""
-                    Oh no! Sorry and Thanks for your valuable feedback.
-                """)
+    col1, col2 = st.columns([1,1])
+    with col1:
+        with st.expander("YES"):
+            st.write("""
+                Thanks for your valuable feedback.
+            """)
+    with col2:
+        with st.expander("NO"):
+            st.write("""
+                Oh no! Sorry and Thanks for your valuable feedback.
+            """)
                 
 if __name__ == "__main__":
     st_ui()
